3461e54cd26fa9120a762d001f7edd26c07441f2
Naomi1745698 <50894381+Naomi1745698@users.noreply.github.com>
2019-05-26 03:52:54 +0930

    Update meteor.h

--- meteor.h
+++ meteor.h
@@ -3,20 +3,28 @@
 #include "astroObjects.h"
 
 
-//Meteor class object
-//Moves around map automatically
-//Damages encountered spaceship
-//Damage dealt is based on meteor size
+//meteors move around the map randomly by 1-block each move
+//if a spaceship hits the meteor, health is deducted (this function is on the spaceship.h file)
+//damage delt is based on meteor size (1,2, or 3)
+
 
 
 class meteor : public astroObjects {
+private:
+	int size;			//1 = smol, 1 damage. 2 = medium, 2 damage, 3 = big, 3 damagae
+
 public:
+	//constructors
 	meteor();
-	meteor(int, int, int);
+	meteor(int size, int rowCoordinateInput, int columnCoordinateInput);
+
+	//getters
+	int getSize();
+
+	//setters
+	void setSize(int meteorSize);
+
+	//constructors
 	~meteor();
-	int getHealth();
-	int getResources();
-	void setHealth(int);
-	void setResources(int);
 };
 #endif
